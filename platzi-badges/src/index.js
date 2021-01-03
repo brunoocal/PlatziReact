f9a7dd2d5d77737e55ae58d2f5883cd669fe59e2
Bruno Gabriel Cal Olivera <75707587+brunoocal@users.noreply.github.com>
2021-01-02 23:29:24 -0300

    Commit #1 - ReactDOM

--- platzi-badges/src/index.js
+++ platzi-badges/src/index.js
@@ -1,16 +1,29 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+
+// const jsx = <h1>Hello, Platzi Badges!</h1>;
+
+// const element = React.createElement('a', 
+// {
+//     href:'https://platzi.com'
+// }, 'Ir a platzi');
+
+const name = "Mark";
+
+// const element = React.createElement('h1', {}, `Hola, soy ${name}`);
+
+// const jsx = <h1>Hola soy, {name}</h1>;
+
+const jsx = (
+    <div>
+        <h1>Hola soy, {name} </h1>
+        <p>Estudiando React</p>
+    </div>
+);
+
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
