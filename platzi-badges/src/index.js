f4b8051d46cb7c351217fe218f9ea93817e65f6f
Bruno Gabriel Cal Olivera <75707587+brunoocal@users.noreply.github.com>
2021-01-03 01:06:00 -0300

    Commit 3# - Using props in React

--- platzi-badges/src/index.js
+++ platzi-badges/src/index.js
@@ -5,14 +5,9 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './global.css';
 
 import Badge from './components/Badge'
+import BadgeNew from './pages/BadgeNew'
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(<Badge 
-    firstName="Brunooh" 
-    lastName="Cal" 
-    jobTitle="Frontend Student" 
-    twitter="Brunoocal"
-    avatar="https://s.gravatar.com/avatar/01c1f19f59da0fd807e885cd60958f53?s=80"
-    />, container);
+ReactDOM.render(<BadgeNew />, container);
